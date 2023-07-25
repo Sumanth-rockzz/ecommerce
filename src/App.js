4081b9dcf4919299fb25aade6f5948cd47de9c3b
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-07-25 09:51:33 -0700

    added Simple Layout home page and cart modal

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import NavBar from "./components/Navbar";
+import StorePage from "./components/Store";
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Header/>
+      <StorePage/>
+    </div>
+  )
+}
+
+export default App;
